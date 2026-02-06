042fb11fedff24cc577b3e2e975a91ac87ee37e3
07kanthraju <07kanthraj@gmail.com>
2026-02-06 23:55:21 +0530

    backend spring boot file structure created

---
+++ backend/src/main/java/com/backend/backend/controller/InsuranceProviderController.java
@@ -0,0 +1,9 @@
+package com.backend.backend.controller;
+
+public class InsuranceProviderController {
+    private int id;
+    private String Name;
+    private String Specialization;
+    private String Address;
+
+}
---
+++ backend/src/main/java/com/backend/backend/repo/PatientRepo.java
@@ -0,0 +1,4 @@
+package com.backend.backend.repo;
+
+public class PatientRepo {
+}
